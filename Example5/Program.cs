fdd70ee9be02f628f0a672716bcde5deead15310
Zhanna <zh_23@list.ru>
2023-12-04 21:50:18 +0300

    прог приветствие любимчика

---
+++ Example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите ваше имя: ");
+string UserName = Console.ReadLine();
+
+if(UserName.ToLower()=="zhanna")
+{
+    Console.WriteLine("Ура,это Zhanna!");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.Write(UserName);
+}
