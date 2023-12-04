17c19e9fd2a0704925a3d48e748805911ba6c50d
Zhanna <zh_23@list.ru>
2023-12-04 22:15:29 +0300

    задача нахождение максимума в данных числах

---
+++ Example6/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 1;
+int b = 8;
+int c = 33;
+int d = 12;
+int e = 6;
+int m = a;
+if(b>m) m=b;
+if (c>m) m=c;
+if (d>m) m=d;
+if (e>m) m=e;
+Console.Write("максимальное ");
+Console.Write(m);
+
